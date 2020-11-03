493fe5dcdab711fd2056ea2a1e517a693a8dccfa
Ashmit <hamal.ashmit@gmail.com>
2020-11-03 13:30:12 +0545

    added safe area

--- lib/main.dart
+++ lib/main.dart
@@ -33,76 +33,79 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-              floating: false,
-              pinned: true,
-              snap: false,
-              expandedHeight: 125,
-              flexibleSpace: Stack(
-                alignment: AlignmentDirectional.topStart,
-                fit: StackFit.expand,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/black.png"),
-                        fit: BoxFit.cover,
+      body: SafeArea(
+        top: false,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+                floating: false,
+                pinned: true,
+                snap: false,
+                expandedHeight: 125,
+                flexibleSpace: Stack(
+                  alignment: AlignmentDirectional.topStart,
+                  fit: StackFit.expand,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/black.png"),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(2.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                          color: Colors.white,
-                          icon: Icon(Icons.arrow_back),
-                          onPressed: () {},
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Text(
-                            "My Requests",
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
+                    Padding(
+                      padding: EdgeInsets.all(2.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            color: Colors.white,
+                            icon: Icon(Icons.arrow_back),
+                            onPressed: () {},
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text(
+                              "My Requests",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                           ),
-                        ),
-                        Row(
-                          children: [
-                            IconButton(
-                              color: Colors.white,
-                              icon: Image.asset(
-                                "assets/settings.png",
-                                width: 25.0,
-                                height: 25.0,
+                          Row(
+                            children: [
+                              IconButton(
+                                color: Colors.white,
+                                icon: Image.asset(
+                                  "assets/settings.png",
+                                  width: 25.0,
+                                  height: 25.0,
+                                ),
+                                onPressed: () {},
                               ),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              color: Colors.white,
-                              icon: Icon(Icons.notifications_none),
-                              onPressed: () {},
-                            ),
-                          ],
-                        )
-                      ],
+                              IconButton(
+                                color: Colors.white,
+                                icon: Icon(Icons.notifications_none),
+                                onPressed: () {},
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              )),
-          SliverList(
-            delegate: SliverChildBuilderDelegate((_, i) {
-              return ListTile(title: Text("Item $i"));
-            }, childCount: 20),
-          ),
-        ],
+                  ],
+                )),
+            SliverList(
+              delegate: SliverChildBuilderDelegate((_, i) {
+                return ListTile(title: Text("Item $i"));
+              }, childCount: 20),
+            ),
+          ],
+        ),
       ),
     );
   }
